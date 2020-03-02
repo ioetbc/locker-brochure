976b420d2e3ed8de85ccdc5c11c5184a47dd58b7
William Cole <williamcole@Williams-MBP-3.lan>
2020-03-02 23:30:55 +0000

    bluk of the site

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import file from './app.zip';
+import Logo from './images/hero-logo.svg';
+import Video from './images/Webvideo.mp4';
+import Background from './images/background.svg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+		<div className="umm">
+			<object className="background" data={Background} type="image/svg+xml" />
+			</div>
+
+			<header>
+				<div className="hero-container">
+					<object className="hero-logo" data={Logo} type="image/svg+xml" />
+					<p className="hero-sub">An application that will lock<br />your computer seconds after<br />you leave your desk</p>
+					<a className="hero-button" href={file} download="">Download for IOS</a>
+				</div>
+			</header>
+
+			<section className="who-container">
+				<h3 className="heading">Who's it for?</h3>
+				<p className="sub-text">Designed for offices that deal with sensitive infomation or have to adhere to iso9000 regulations, WorkLock will effectively lock a computer in the case of an employee forgetting to lock their computer when leaving their desk.</p>
+			</section>
+
+			<section className="who-container">
+				<h3 className="heading">How does it work?</h3>
+				<div className="who-video">
+				<video width="100%" height="100%" controls autoplay>
+					  <source src={Video} type="video/mp4"></source>
+				</video>
+				</div>
+			</section>
+		</div>
+	);
 }
 
 export default App;
